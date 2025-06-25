005a76013317477a6d9cf7ca0a08b2f96ef9d6db
mdaurangzeb398 <mdaurangzeb398@gmail.com>
2025-06-25 10:31:02 +0530

    Create social_platform_keys.dart

---
+++ social_platform_keys.dart
@@ -0,0 +1,8 @@
+// 📄 lib/secrets/social_platform_keys.dart
+
+class SocialKeys {
+  static const facebookPageId = "fb_123456";
+  static const facebookAccessToken = "EAAbcd...xyz";
+  static const youtubeApiKey = "AIza...xyz";
+  static const telegramBotToken = "123456:ABC...DEF";
+}
